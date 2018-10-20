25ea2539505aeeee629e1883ebdf22323e889766
Gad Arenas <garenas@sysware.com.mx>
2018-10-20 16:40:14 -0500

    Cambios

---
+++ resources/views/emails/register.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Registro de usuario</h2>
+        <p>Usuario: {{ $data['user'] }}</p>
+        <p>Contraseña: {{ $data['password'] }}</p>
+    </body>
+</html>
